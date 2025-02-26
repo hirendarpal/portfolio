1c0925bd75bd1f91f94efa4e359225175c93ee3d
hirendarpal <palhirendar@gmail.com>
2025-02-27 00:41:45 +0530

    height2 plus

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -84,7 +84,7 @@ const Contact = () => {
         pauseOnHover
         theme="dark"
       />
-    <div className="flex justify-center items-center min-h-screen mt-12 md:mt-18">
+    <div className="flex justify-center items-center min-h-screen mt-14 md:mt-18">
       <form 
         ref={form}
         id='myform'
